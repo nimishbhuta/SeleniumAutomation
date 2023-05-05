9179cfc56a8d802243d8fc1e7abc6fbd96a68b99
Bhuta <nimish.bhuta@sogeti.com>
2023-05-05 14:41:16 +0200

    first commit selenium

---
+++ src/test/java/org/selenium/SimpleSeleniumTest.java
@@ -0,0 +1,24 @@
+package org.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class SimpleSeleniumTest {
+
+    @Test
+    public void openBrowser(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("http://www.google.com");
+        driver.quit();
+    }
+
+
+
+
+}
